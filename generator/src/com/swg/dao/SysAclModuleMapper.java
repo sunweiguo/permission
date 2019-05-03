5ada76b329f34089ba13f36f2d8525f7f036564f
sunweiguo <317758022@qq.com>
2019-05-03 17:11:49 +0800

    '基础工具类实现'

---
+++ generator/src/com/swg/dao/SysAclModuleMapper.java
@@ -0,0 +1,17 @@
+package com.swg.dao;
+
+import com.swg.entity.SysAclModule;
+
+public interface SysAclModuleMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysAclModule record);
+
+    int insertSelective(SysAclModule record);
+
+    SysAclModule selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysAclModule record);
+
+    int updateByPrimaryKey(SysAclModule record);
+}
---
+++ generator/src/com/swg/dao/SysDeptMapper.java
@@ -0,0 +1,17 @@
+package com.swg.dao;
+
+import com.swg.entity.SysDept;
+
+public interface SysDeptMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysDept record);
+
+    int insertSelective(SysDept record);
+
+    SysDept selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SysDept record);
+
+    int updateByPrimaryKey(SysDept record);
+}
